b4b0b5ee104c26798234852d79cca5aa81036172
maurogarzia <maurogarzia2@gmail.com>
2024-10-15 16:14:58 -0300

    Subo ModalViewCompany con modificacion de logo

--- Proyecto Final Programacion 3 UTN/src/components/UI/ModalViewCompany/ModalViewCompany.tsx
+++ Proyecto Final Programacion 3 UTN/src/components/UI/ModalViewCompany/ModalViewCompany.tsx
@@ -21,6 +21,17 @@ const ModalViewCompany : FC<IModalViewCompany>  = ({company, modalClose}) =>{
                 <p>Razon Social: {company.social_razon} </p>
                 <p>Cuit: {company.cuit}</p>
                 <p>Logo: </p>
+                <div>
+                <img 
+            src={
+                company.logo instanceof File 
+                ? URL.createObjectURL(company.logo)  // Si es un archivo File
+                : company.logo 
+                  ? company.logo  // Si es una cadena de texto
+                  : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQppJKxBxJI-9UWLe2VVmzuBd24zsq4_ihxZw&s"  // Si es null o undefined
+            }  
+            alt="" />
+                </div>
             </div>
             <div className={styles.containerButton}>
                 <Button onClick={modalClose}>Cerrar</Button>
