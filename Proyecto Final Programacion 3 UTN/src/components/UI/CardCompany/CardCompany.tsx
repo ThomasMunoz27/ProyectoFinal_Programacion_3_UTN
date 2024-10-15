2a74c0ab1b18b203ba4e5a073e0d90bc2367ca52
ThomasMunoz27 <thomasnm2004@gmail.com>
2024-10-15 10:05:33 -0300

    pequeños cambios

--- Proyecto Final Programacion 3 UTN/src/components/UI/CardCompany/CardCompany.tsx
+++ Proyecto Final Programacion 3 UTN/src/components/UI/CardCompany/CardCompany.tsx
@@ -9,16 +9,16 @@ interface ICardCompany{
 
 export const CardCompany : FC<ICardCompany>= ({company}) => {
   return (
-    <Card className={styles.card} style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>{company.name}</Card.Title>
-      <Card.Subtitle>Razon social: {company.social_razon}</Card.Subtitle>
-      
-      <div className={styles.buttonContainer}>
-            <Button>Ver</Button>
-            <Button>Editar</Button>
-      </div>
-    </Card.Body>
+    <Card className={styles.cardContainer}>
+      <Card.Body>
+        <Card.Title>{company.name}</Card.Title>
+        <Card.Subtitle>Razon social: {company.social_razon}</Card.Subtitle>
+        
+        <div className={styles.buttonContainer}>
+              <Button>Ver</Button>
+              <Button>Editar</Button>
+        </div>
+      </Card.Body>
   </Card>
   )
 }
