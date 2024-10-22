8b43195222a0f216cb39ac34507180b2bdfe8177
ThomasMunoz27 <thomasnm2004@gmail.com>
2024-10-22 13:19:33 -0300

    implementado un boton para navegar provisoriamente  a administracion desde Home

--- Proyecto Final Programacion 3 UTN/src/components/UI/HomeComps/HomeHeader/Header.tsx
+++ Proyecto Final Programacion 3 UTN/src/components/UI/HomeComps/HomeHeader/Header.tsx
@@ -1,8 +1,19 @@
+import { Button } from "react-bootstrap";
 import styles from "./Header.module.css";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+
+  const navigate = useNavigate()
+
+  const handleNavigateToAdmin = ()=>{
+    navigate(`/administracion`)
+  }
     // Me gustaría que al apretar adminEmpresas.com vuelva al inicio.
   return (
-    <header className={styles.header}><h1 className={styles.header__logo}>adminEmpresas.com</h1></header>
+    <header className={styles.header}>
+      <h1 className={styles.header__logo}>adminEmpresas.com</h1>
+      <Button onClick={handleNavigateToAdmin} variant="primary">Ir a administracion</Button>
+    </header>
   )
 }
--- Proyecto Final Programacion 3 UTN/src/routes/AppRouter.tsx
+++ Proyecto Final Programacion 3 UTN/src/routes/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Header } from "../components/UI/HomeComps/HomeHeader/Header"
 
 import { Home } from "../components/screens/Home/Home"
+import { Administration } from "../components/screens/Administration/Index/Administration"
 
 
 
@@ -14,7 +15,7 @@ export const AppRouter = () => {
     <Routes>
 
         <Route path="/" element={<Home/>}></Route>
-
+        <Route path="/administracion" element={<Administration/>} />
         
 
     </Routes>
