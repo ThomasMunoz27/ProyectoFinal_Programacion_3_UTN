83d2edbb5443a6e68152e3ec6a35c3897a7335d2
erredev-JS <roddev100@gmail.com>
2024-10-28 12:45:05 -0300

    ViewSucursals Styles

--- Proyecto Final Programacion 3 UTN/src/components/UI/HomeComps/CardSucursals/CardSucursal.tsx
+++ Proyecto Final Programacion 3 UTN/src/components/UI/HomeComps/CardSucursals/CardSucursal.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react"
 import { ISucursal } from "../../../../types/dtos/sucursal/ISucursal"
 import { Button, Card } from "react-bootstrap"
-
+import styles from '../CardSucursals/CardSucursal.module.css'
 
 interface ICardSucursal {
     sucursal: ISucursal
@@ -10,16 +10,16 @@ interface ICardSucursal {
 export const CardSucursal: FC<ICardSucursal> = ({sucursal}) => {
   return (
     <>
-        <Card style={{ width: '18rem' }}>
+        <Card className={styles.card}>
             <Card.Body>
-                <img 
+                <img className={styles.cardImg} 
                     src={
                     sucursal.logo ? sucursal.logo : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQppJKxBxJI-9UWLe2VVmzuBd24zsq4_ihxZw&s"  // Si es null o undefined
                     }  
                     alt="" />
                 <Card.Title>{sucursal.nombre}</Card.Title>
                 <Card.Subtitle>{sucursal.horarioApertura} - {sucursal.horarioCierre}</Card.Subtitle>
-                <div >
+                <div className={styles.containerButtons}>
                     <Button >Ver</Button>
                     <Button >Editar</Button>
                 </div>
