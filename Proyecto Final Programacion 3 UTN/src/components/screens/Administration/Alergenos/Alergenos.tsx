ff397cd0679d1bce28a96c7c04902fe446621637
erredev-JS <roddev100@gmail.com>
2024-11-05 19:40:22 -0300

    Alergenos 50%

--- Proyecto Final Programacion 3 UTN/src/components/screens/Administration/Alergenos/Alergenos.tsx
+++ Proyecto Final Programacion 3 UTN/src/components/screens/Administration/Alergenos/Alergenos.tsx
@@ -1,78 +1,77 @@
+import { Button } from "react-bootstrap";
+import { RootState } from "../../../../redux/store/store";
+import { alergenoService } from "../../../../Services/alergenoServices";
+import { IAlergenos } from "../../../../types/dtos/alergenos/IAlergenos";
 import { AdministrationAside } from "../../../UI/Administration/Aside/AdministrationAside";
 import { AdministrationHeader } from "../../../UI/Administration/Header/AdministrationHeader";
 import styles from "./Alergenos.module.css";
-import { FC, MouseEventHandler, useState } from "react";
+import { FC, MouseEventHandler, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-// Función para mostrar alérgenos (no está implementada)
-
-
-
-const BodyDeploy = () => {
-  return (
-    <div className={styles.BodyDeploy}>
-      {/* Aquí puedes añadir contenido para el cuerpo */}
-    </div>
-  );
-};
-
-
-type AddAlergenButtonProps = {
-  onClick: MouseEventHandler<HTMLButtonElement>;
-  closeModal: () => void; // Aquí
-};
-
-type ModalAddAlergenProps = {
-  closeModal: () => void;
-  isVisible: boolean;
-};
-
-const ModalAddAlergen: FC<ModalAddAlergenProps> = ({ isVisible, closeModal }) => {
-  if (!isVisible) return null;
-
-  const handleCancel = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault(); // Evita la recarga de la página
-    closeModal(); // Cierra el modal
-  };
-
-  return (
-    <form className={`${styles.ModalAddAlergen} ${isVisible ? styles.ModalAddAlergen : styles.hidden}`}>
-      <div className={styles.ModalAddAlergenTitle}>Crear un alergeno</div>
-      <input type="text" placeholder="Ingresa una denominación" className={styles.ModalAddAlergenInputDen} />
-      <div className={styles.ModalAddAlergenImageSelector}>
-        Eliga una imagen:
-        <input type="file" />
-        <img className={styles.ModalAddAlergenImage} src="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=no_photography" />
-      </div>
-      <div className={styles.ModalAddAlergenButtons}>
-        <button className={styles.ModalCancel} onClick={handleCancel}>CANCELAR</button>
-        <button type="submit" className={styles.ModalConfirm}>CONFIRMAR</button>
-      </div>
-    </form>
-  );
-};
-
-const AddAlergenButton: FC<AddAlergenButtonProps> = ({ onClick }) => {
-  return (
-    <button className={styles.AddAlergen} onClick={onClick}>Agregar un alergeno</button>
-  );
-};
-
-export const Alergenos = () => {
+export const Alergenos: FC = () => {
+  const [alergenos, setAlergenos] = useState<IAlergenos[]>([]);
   const [isModalVisible, setModalVisible] = useState(false);
+
+  const storedSucursal = localStorage.getItem('sucursal');
+  const selectedSucursal = storedSucursal
+    ? JSON.parse(storedSucursal)
+    : useSelector((state: RootState) => state.sucursal.selectedSucursal);
+
+  useEffect(() => {
+    const fetchAlergenos = async () => {
+      const data = await alergenoService.getAllAlergenos();
+      setAlergenos(data);
+    };
+
+    fetchAlergenos();
+  }, []);
 
   const openModal = () => setModalVisible(true);
   const closeModal = () => setModalVisible(false);
 
-  const MainDiv: FC = () => {
-    return (
-      <div className={styles.MainDiv}>
-        <div className={styles.titleContainer}>
+  const BodyDeploy = () => (
+    <div className={styles.BodyDeploy}>
+      <ul className={styles.containerList}>
+        {alergenos.map((alergeno) => (
+          <li key={alergeno.id} className={styles.containerItem}>
+            {alergeno.denominacion} 
+            <div className={styles.containerButtons}>
+            <Button>👁</Button><Button>✏</Button><Button>🗑</Button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 
-        <h1>Alergenos</h1>
-        <AddAlergenButton onClick={openModal} closeModal={closeModal} /> {/* Aquí se pasa closeModal */}
+  const AddAlergenButton: FC<{ onClick: MouseEventHandler<HTMLButtonElement> }> = ({ onClick }) => (
+    <button className={styles.AddAlergen} onClick={onClick}>
+      Agregar un alergeno
+    </button>
+  );
+
+  const ModalAddAlergen: FC<{ isVisible: boolean; closeModal: () => void }> = ({ isVisible, closeModal }) => {
+    if (!isVisible) return null;
+
+    const handleCancel = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+      closeModal();
+    };
+
+    return (
+      <form className={`${styles.ModalAddAlergen} ${isVisible ? styles.ModalAddAlergen : styles.hidden}`}>
+        <div className={styles.ModalAddAlergenTitle}>Crear un alergeno</div>
+        <input type="text" placeholder="Ingresa una denominación" className={styles.ModalAddAlergenInputDen} />
+        <div className={styles.ModalAddAlergenImageSelector}>
+          Elija una imagen:
+          <input type="file" />
+          <img className={styles.ModalAddAlergenImage} src="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=no_photography" />
         </div>
-        <BodyDeploy />
-      </div>
+        <div className={styles.ModalAddAlergenButtons}>
+          <button className={styles.ModalCancel} onClick={handleCancel}>CANCELAR</button>
+          <button type="submit" className={styles.ModalConfirm}>CONFIRMAR</button>
+        </div>
+      </form>
     );
   };
 
@@ -81,13 +80,17 @@ export const Alergenos = () => {
       <AdministrationHeader />
       <div className={styles.mainContent}>
         <AdministrationAside />
-
-       
-
-        <MainDiv />
+        <div className={styles.MainDiv}>
+          <div className={styles.titleContainer}>
+            <h1>Alergenos</h1>
+            <AddAlergenButton onClick={openModal} />
+          </div>
+          <BodyDeploy />
+        </div>
         <ModalAddAlergen isVisible={isModalVisible} closeModal={closeModal} />
-
       </div>
     </>
   );
 };
+
+export default Alergenos;
